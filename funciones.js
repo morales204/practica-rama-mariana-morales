5e51662bccb766cdf30d73d4249fdcf532362ac5
morales204 <marianamoralesrodriguez25@gmail.com>
2025-10-15 15:32:32 -0600

    Cambio en funciones

--- funciones.js
+++ funciones.js
@@ -2,6 +2,7 @@ function saludar (){
     console.log("Hola Mundo");
 }
 
+// Función para validar un correo electrónico
 function validarEmail(email) {
     return re.test(String(email).toLowerCase());
 }
